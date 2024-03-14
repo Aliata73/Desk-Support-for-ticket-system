ad16bf1fd2c441ce9eaaa11a328fc05e696cc7cb
aliata73 <aliata73@protonmail.com>
2024-03-14 11:29:04 +0330

    commit2

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
-console.log('server');
+const express = require('express')
+const colors = require('colors')
+require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+
+//connect to mongoDB
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.json({message: 'This is from golden api to your welcom'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
